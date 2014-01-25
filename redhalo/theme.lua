b4c6c3b716d6021682f8ae5e14c8334addd053bf
max demian <mikar gmx de>
2014-01-25 17:44:10 +0100

    fixed zenburn-red and changed redhalo

--- redhalo/theme.lua
+++ redhalo/theme.lua
@@ -28,15 +28,15 @@ wallpaper4    = sharedthemes .. "/default/background.png"
 wpscript      = home .. "/.wallpaper"
 
 if awful.util.file_readable(wallpaper1) then
-	theme.wallpaper = wallpaper1
+  theme.wallpaper = wallpaper1
 elseif awful.util.file_readable(wallpaper2) then
-	theme.wallpaper = wallpaper2
+  theme.wallpaper = wallpaper2
 elseif awful.util.file_readable(wpscript) then
-	theme.wallpaper_cmd = { "sh " .. wpscript }
+  theme.wallpaper_cmd = { "sh " .. wpscript }
 elseif awful.util.file_readable(wallpaper3) then
-	theme.wallpaper = wallpaper3
+  theme.wallpaper = wallpaper3
 else
-	theme.wallpaper = wallpaper4
+  theme.wallpaper = wallpaper4
 end
 
 if awful.util.file_readable(config .. "/vain/init.lua") then
@@ -44,7 +44,7 @@ if awful.util.file_readable(config .. "/vain/init.lua") then
 end
 --}}}
 
-theme.font = "profont 8"
+theme.font = "Dejavu Sans 8"
 
 theme.fg_normal = "#bcbcbc"
 --theme.fg_normal = "#f8f8f8"
@@ -59,9 +59,9 @@ theme.bg_urgent = "#f92671"
 theme.bg_disabled = "#5e7175"
 theme.bg_hover = "#5e7175"
 
-theme.border_width = 0
+theme.border_width = 1
 theme.border_normal = "#505050"
-theme.border_focus = "#292929"
+theme.border_focus = "#5e7175" --#292929"
 theme.border_marked = "#ce5666"
 
 --{{{ Taglist icons
@@ -73,22 +73,33 @@ theme.border_marked = "#ce5666"
 theme.tasklist_floating_icon = themedir .. "/layouts-huge/floating-greyish.png"
 --}}}
 -- {{{ Layout icons
-theme.layout_tile       = themedir .. "/layouts-huge/tile-blue.png"
-theme.layout_tileleft   = themedir .. "/layouts-huge/tileleft-red.png"
-theme.layout_tilebottom = themedir .. "/layouts-huge/tilebottom-green.png"
-theme.layout_tiletop    = themedir .. "/layouts-huge/tiletop-blue.png"
-theme.layout_fairv      = themedir .. "/layouts-huge/fairv-red.png"
-theme.layout_fairh      = themedir .. "/layouts-huge/fairh-green.png"
-theme.layout_spiral     = themedir .. "/layouts-huge/spiral-blue.png"
-theme.layout_dwindle    = themedir .. "/layouts-huge/dwindle-red.png"
-theme.layout_max        = themedir .. "/layouts-huge/max-green.png"
-theme.layout_fullscreen = themedir .. "/layouts-huge/fullscreen-blue.png"
-theme.layout_magnifier  = themedir .. "/layouts-huge/magnifier-red.png"
-theme.layout_floating   = themedir .. "/layouts-huge/floating-green.png"
+theme.layout_tile           = themedir .. "/layouts-huge/tile-blue.png"
+theme.layout_tileleft       = themedir .. "/layouts-huge/tileleft-blue.png"
+theme.layout_tilebottom     = themedir .. "/layouts-huge/tilebottom-blue.png"
+theme.layout_tiletop        = themedir .. "/layouts-huge/tiletop-blue.png"
+theme.layout_fairv          = themedir .. "/layouts-huge/fairv-green.png"
+theme.layout_fairh          = themedir .. "/layouts-huge/fairh-green.png"
+theme.layout_spiral         = themedir .. "/layouts-huge/spiral-blue.png"
+theme.layout_dwindle        = themedir .. "/layouts-huge/dwindle-red.png"
+theme.layout_max            = themedir .. "/layouts-huge/max-green.png"
+theme.layout_fullscreen     = themedir .. "/layouts-huge/fullscreen-blue.png"
+theme.layout_magnifier      = themedir .. "/layouts-huge/magnifier-red.png"
+theme.layout_floating       = themedir .. "/layouts-huge/floating-green.png"
+
+theme.layout_uselesstile        = themedir .. "/layouts-huge/tile-blue.png"
+theme.layout_uselesstileleft    = themedir .. "/layouts-huge/tileleft-blue.png"
+theme.layout_uselesstilebottom  = themedir .. "/layouts-huge/tilebottom-blue.png"
+theme.layout_uselesstiletop     = themedir .. "/layouts-huge/tiletop-blue.png"
+theme.layout_uselessfairv       = themedir .. "/layouts-huge/fairv-green.png"
+theme.layout_uselessfairh       = themedir .. "/layouts-huge/fairh-green.png"
+theme.layout_centerwork         = themedir .. "/layouts-huge/centerwork-blue.png"
+theme.layout_cascade            = themedir .. "/layouts-huge/cascade.png"
+theme.layout_cascadebrowse      = themedir .. "/layouts-huge/cascadebrowse.png"
+
 -- }}}
 
 -- {{{ Titlebar icons
-theme.titlebar_close_button_focus	    = themedir .. "/titlebar/close_focus-darkbrown.png"
+theme.titlebar_close_button_focus     = themedir .. "/titlebar/close_focus-darkbrown.png"
 theme.titlebar_ontop_button_focus_active    = themedir .. "/titlebar/ontop_focus_active-darkbrown.png"
 theme.titlebar_ontop_button_focus_inactive  = themedir .. "/titlebar/ontop_focus_inactive-darkbrown.png"
 theme.titlebar_sticky_button_focus_active    = themedir .. "/titlebar/sticky_focus_active-darkbrown.png"
