4d7c8f895ce37f04ef922c2eb529a24d307a93d4
Max Falk <gmdfalk@gmail.com>
2019-05-26 17:24:40 +0200

    update themes

--- dremora/theme.lua
+++ dremora/theme.lua
@@ -1,97 +1,253 @@
 --[[
-                                   
-     Dremora Awesome WM config 2.0 
-     github.com/copycat-killer     
-                                   
+
+     Dremora Awesome WM theme 2.0
+     github.com/lcpz
+
 --]]
 
+local gears = require("gears")
+local lain  = require("lain")
 local awful = require("awful")
-awful.util = require("awful.util")
+local wibox = require("wibox")
+local dpi   = require("beautiful.xresources").apply_dpi
 
---{{{ Main
-theme = {}
+local os = os
+local my_table = awful.util.table or gears.table -- 4.{0,1} compatibility
 
-home          = os.getenv("HOME")
-config        = awful.util.getdir("config")
-shared        = "/usr/share/awesome"
-if not awful.util.file_readable(shared .. "/icons/awesome16.png") then
-    shared    = "/usr/share/local/awesome"
+local theme                                     = {}
+theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/dremora"
+theme.wallpaper                                 = theme.dir .. "/wall.png"
+theme.font                                      = "Misc Tamsyn 10.5"
+theme.taglist_font                              = "Icons 10"
+theme.fg_normal                                 = "#747474"
+theme.fg_focus                                  = "#DDDCFF"
+theme.bg_normal                                 = "#121212"
+theme.bg_focus                                  = "#121212"
+theme.fg_urgent                                 = "#CC9393"
+theme.bg_urgent                                 = "#2A1F1E"
+theme.border_width                              = dpi(0)
+theme.border_normal                             = "#121212"
+theme.border_focus                              = "#292929"
+theme.titlebar_bg_focus                         = "#292929"
+theme.taglist_fg_focus                          = "#dddcff"
+theme.taglist_bg_focus                          = "#121212"
+theme.menu_height                               = dpi(16)
+theme.menu_width                                = dpi(130)
+theme.menu_submenu_icon                         = theme.dir .. "/icons/submenu.png"
+theme.awesome_icon                              = theme.dir .."/icons/awesome.png"
+theme.taglist_squares_sel                       = theme.dir .. "/icons/square_sel.png"
+theme.taglist_squares_unsel                     = theme.dir .. "/icons/square_unsel.png"
+theme.layout_tile                               = theme.dir .. "/icons/tile.png"
+theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
+theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
+theme.layout_tiletop                            = theme.dir .. "/icons/tiletop.png"
+theme.layout_fairv                              = theme.dir .. "/icons/fairv.png"
+theme.layout_fairh                              = theme.dir .. "/icons/fairh.png"
+theme.layout_spiral                             = theme.dir .. "/icons/spiral.png"
+theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.png"
+theme.layout_max                                = theme.dir .. "/icons/max.png"
+theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
+theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
+theme.layout_floating                           = theme.dir .. "/icons/floating.png"
+theme.tasklist_plain_task_name                  = true
+theme.tasklist_disable_icon                     = true
+theme.useless_gap                               = dpi(10)
+theme.titlebar_close_button_focus               = theme.dir .. "/icons/titlebar/close_focus.png"
+theme.titlebar_close_button_normal              = theme.dir .. "/icons/titlebar/close_normal.png"
+theme.titlebar_ontop_button_focus_active        = theme.dir .. "/icons/titlebar/ontop_focus_active.png"
+theme.titlebar_ontop_button_normal_active       = theme.dir .. "/icons/titlebar/ontop_normal_active.png"
+theme.titlebar_ontop_button_focus_inactive      = theme.dir .. "/icons/titlebar/ontop_focus_inactive.png"
+theme.titlebar_ontop_button_normal_inactive     = theme.dir .. "/icons/titlebar/ontop_normal_inactive.png"
+theme.titlebar_sticky_button_focus_active       = theme.dir .. "/icons/titlebar/sticky_focus_active.png"
+theme.titlebar_sticky_button_normal_active      = theme.dir .. "/icons/titlebar/sticky_normal_active.png"
+theme.titlebar_sticky_button_focus_inactive     = theme.dir .. "/icons/titlebar/sticky_focus_inactive.png"
+theme.titlebar_sticky_button_normal_inactive    = theme.dir .. "/icons/titlebar/sticky_normal_inactive.png"
+theme.titlebar_floating_button_focus_active     = theme.dir .. "/icons/titlebar/floating_focus_active.png"
+theme.titlebar_floating_button_normal_active    = theme.dir .. "/icons/titlebar/floating_normal_active.png"
+theme.titlebar_floating_button_focus_inactive   = theme.dir .. "/icons/titlebar/floating_focus_inactive.png"
+theme.titlebar_floating_button_normal_inactive  = theme.dir .. "/icons/titlebar/floating_normal_inactive.png"
+theme.titlebar_maximized_button_focus_active    = theme.dir .. "/icons/titlebar/maximized_focus_active.png"
+theme.titlebar_maximized_button_normal_active   = theme.dir .. "/icons/titlebar/maximized_normal_active.png"
+theme.titlebar_maximized_button_focus_inactive  = theme.dir .. "/icons/titlebar/maximized_focus_inactive.png"
+theme.titlebar_maximized_button_normal_inactive = theme.dir .. "/icons/titlebar/maximized_normal_inactive.png"
+
+awful.util.tagnames   = { "ƀ", "Ƅ", "Ɗ", "ƈ", "ƙ" }
+
+local markup     = lain.util.markup
+local separators = lain.util.separators
+local white      = theme.fg_focus
+local gray       = "#858585"
+
+-- Textclock
+local mytextclock = wibox.widget.textclock(markup(gray, " %a")
+.. markup(white, " %d ") .. markup(gray, "%b ") ..  markup(white, "%H:%M "))
+mytextclock.font = theme.font
+
+-- Calendar
+theme.cal = lain.widget.cal({
+    attach_to = { mytextclock },
+    notification_preset = {
+        font = "Misc Tamsyn 11",
+        fg   = white,
+        bg   = theme.bg_normal
+}})
+
+-- Mail IMAP check
+--[[ commented because it needs to be set before use
+theme.mail = lain.widget.imap({
+    timeout  = 180,
+    server   = "server",
+    mail     = "mail",
+    password = "keyring get mail",
+    notification_preset = { fg = white }
+    settings = function()
+        mail  = ""
+        count = ""
+
+        if mailcount > 0 then
+            mail = "Mail "
+            count = mailcount .. " "
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, mail) .. markup(white, count)))
+    end
+})
+--]]
+
+-- MPD
+theme.mpd = lain.widget.mpd({
+    settings = function()
+        mpd_notification_preset.fg = white
+        artist = mpd_now.artist .. " "
+        title  = mpd_now.title  .. " "
+
+        if mpd_now.state == "pause" then
+            artist = "mpd "
+            title  = "paused "
+        elseif mpd_now.state == "stop" then
+            artist = ""
+            title  = ""
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, artist) .. markup(white, title)))
+    end
+})
+
+-- /home fs
+--[[ commented because it needs Gio/Glib >= 2.54
+theme.fs = lain.widget.fs({
+    notification_preset = { fg = white, bg = theme.bg_normal, font = "Misc Tamsyn 10.5" },
+    settings  = function()
+        fs_header = ""
+        fs_p      = ""
+
+        if fs_now["/home"].percentage >= 90 then
+            fs_header = " Hdd "
+            fs_p      = fs_now["/home"].percentage
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, fs_header) .. markup(white, fs_p)))
+    end
+})
+--]]
+
+-- Battery
+local bat = lain.widget.bat({
+    settings = function()
+        bat_header = " Bat "
+        bat_p      = bat_now.perc .. " "
+        widget:set_markup(markup.font(theme.font, markup(gray, bat_header) .. markup(white, bat_p)))
+    end
+})
+
+-- ALSA volume
+theme.volume = lain.widget.alsa({
+    --togglechannel = "IEC958,3",
+    settings = function()
+        header = " Vol "
+        vlevel  = volume_now.level
+
+        if volume_now.status == "off" then
+            vlevel = vlevel .. "M "
+        else
+            vlevel = vlevel .. " "
+        end
+
+        widget:set_markup(markup.font(theme.font, markup(gray, header) .. markup(white, vlevel)))
+    end
+})
+
+-- Weather
+theme.weather = lain.widget.weather({
+    city_id = 2643743, -- placeholder (London)
+    notification_preset = { fg = white }
+})
+
+-- Separators
+local first     = wibox.widget.textbox('<span font="Misc Tamsyn 4"> </span>')
+local arrl_pre  = separators.arrow_right("alpha", "#1A1A1A")
+local arrl_post = separators.arrow_right("#1A1A1A", "alpha")
+
+function theme.at_screen_connect(s)
+    -- Quake application
+    s.quake = lain.util.quake({ app = awful.util.terminal })
+
+    -- If wallpaper is a function, call it with the screen
+    local wallpaper = theme.wallpaper
+    if type(wallpaper) == "function" then
+        wallpaper = wallpaper(s)
+    end
+    gears.wallpaper.maximized(wallpaper, s, true)
+
+    -- Tags
+    awful.tag(awful.util.tagnames, s, awful.layout.layouts)
+
+    -- Create a promptbox for each screen
+    s.mypromptbox = awful.widget.prompt()
+    -- Create an imagebox widget which will contains an icon indicating which layout we're using.
+    -- We need one layoutbox per screen.
+    s.mylayoutbox = awful.widget.layoutbox(s)
+    s.mylayoutbox:buttons(my_table.join(
+                           awful.button({}, 1, function () awful.layout.inc( 1) end),
+                           awful.button({}, 2, function () awful.layout.set( awful.layout.layouts[1] ) end),
+                           awful.button({}, 3, function () awful.layout.inc(-1) end),
+                           awful.button({}, 4, function () awful.layout.inc( 1) end),
+                           awful.button({}, 5, function () awful.layout.inc(-1) end)))
+    -- Create a taglist widget
+    s.mytaglist = awful.widget.taglist(s, awful.widget.taglist.filter.all, awful.util.taglist_buttons)
+
+    -- Create a tasklist widget
+    s.mytasklist = awful.widget.tasklist(s, awful.widget.tasklist.filter.currenttags, awful.util.tasklist_buttons)
+
+    -- Create the wibox
+    s.mywibox = awful.wibar({ position = "top", screen = s, height = dpi(18), bg = theme.bg_normal, fg = theme.fg_normal })
+
+    -- Add widgets to the wibox
+    s.mywibox:setup {
+        layout = wibox.layout.align.horizontal,
+        { -- Left widgets
+            layout = wibox.layout.fixed.horizontal,
+            first,
+            s.mytaglist,
+            arrl_pre,
+            s.mylayoutbox,
+            arrl_post,
+            s.mypromptbox,
+            first,
+        },
+        s.mytasklist, -- Middle widget
+        { -- Right widgets
+            layout = wibox.layout.fixed.horizontal,
+            wibox.widget.systray(),
+            first,
+            theme.mpd.widget,
+            --theme.mail.widget,
+            --theme.fs.widget,
+            bat.widget,
+            theme.volume.widget,
+            mytextclock,
+        },
+    }
 end
-sharedicons   = shared .. "/icons"
-sharedthemes  = shared .. "/themes"
-themes        = config .. "/themes"
-themename     = "/dremora"
-if not awful.util.file_readable(themes .. themename .. "/theme.lua") then
-       themes = sharedthemes
-end
-themedir      = themes .. themename
-
-wallpaper1    = themedir .. "/wall.png"
-wallpaper2    = themedir .. "/background.png"
-wallpaper3    = sharedthemes .. "/zenburn/zenburn-background.png"
-wallpaper4    = sharedthemes .. "/default/background.png"
-wpscript      = home .. "/.wallpaper"
-
-if awful.util.file_readable(wallpaper1) then
-  theme.wallpaper = wallpaper1
-elseif awful.util.file_readable(wallpaper2) then
-  theme.wallpaper = wallpaper2
-elseif awful.util.file_readable(wpscript) then
-  theme.wallpaper_cmd = { "sh " .. wpscript }
-elseif awful.util.file_readable(wallpaper3) then
-  theme.wallpaper = wallpaper3
-else
-  theme.wallpaper = wallpaper4
-end
---}}}
-
-theme.font                          = "Tamsyn 10.5"
-theme.taglist_font                  = "Icons 10"
-theme.fg_normal                     = "#747474"
-theme.fg_focus                      = "#DDDCFF"
-theme.bg_normal                     = "#121212"
-theme.bg_focus                      = "#121212"
-theme.fg_urgent                     = "#CC9393"
-theme.bg_urgent                     = "#2A1F1E"
-theme.border_width                  = "0"
-theme.border_normal                 = "#121212"
-theme.border_focus                  = "#292929"
-theme.titlebar_bg_focus             = "#292929"
-
-theme.taglist_fg_focus              = "#dddcff"
-theme.taglist_bg_focus              = "#121212"
-theme.menu_height                   = "16"
-theme.menu_width                    = "140"
-
-theme.menu_submenu_icon             = themedir .. "/icons/submenu.png"
-theme.taglist_squares_sel           = themedir .. "/icons/square_sel.png"
-theme.taglist_squares_unsel         = themedir .. "/icons/square_unsel.png"
-theme.arrl_lr_pre                   = themedir .. "/icons/arrl_lr_pre.png"
-theme.arrl_lr_post                  = themedir .. "/icons/arrl_lr_post.png"
-
-theme.layout_tile                   = themedir .. "/icons/tile.png"
-theme.layout_tilegaps               = themedir .. "/icons/tilegaps.png"
-theme.layout_tileleft               = themedir .. "/icons/tileleft.png"
-theme.layout_tilebottom             = themedir .. "/icons/tilebottom.png"
-theme.layout_tiletop                = themedir .. "/icons/tiletop.png"
-theme.layout_fairv                  = themedir .. "/icons/fairv.png"
-theme.layout_fairh                  = themedir .. "/icons/fairh.png"
-theme.layout_spiral                 = themedir .. "/icons/spiral.png"
-theme.layout_dwindle                = themedir .. "/icons/dwindle.png"
-theme.layout_max                    = themedir .. "/icons/max.png"
-theme.layout_fullscreen             = themedir .. "/icons/fullscreen.png"
-theme.layout_magnifier              = themedir .. "/icons/magnifier.png"
-theme.layout_floating               = themedir .. "/icons/floating.png"
-
-theme.tasklist_disable_icon         = true
-theme.tasklist_floating             = ""
-theme.tasklist_maximized_horizontal = ""
-theme.tasklist_maximized_vertical   = ""
-
--- lain related
-theme.useless_gap_width             = 10
-theme.layout_uselesstile            = themedir .. "/icons/uselesstile.png"
-theme.layout_uselesstileleft        = themedir .. "/icons/uselesstileleft.png"
-theme.layout_uselesstiletop         = themedir .. "/icons/uselesstiletop.png"
 
 return theme
